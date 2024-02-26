2c3e5f62fb90a94d9756b82b01535959e7c7db6a
kswamygm <kumara.swamygm@epsilon.com>
2024-02-26 13:09:04 +0530

    build file added

--- backend/server.js
+++ backend/server.js
@@ -4,20 +4,27 @@ const cors = require("cors");
 require("dotenv").config()
 
 const dbConfig = require("./config/dbConfig")
-const app = express()
-app.use(express.json())
+
 const userRoute = require("./routes/userRoute");
 const movieRoute = require("./routes/movieRoute");
 const theaterRoute = require("./routes/theaterRoute");
 const showRoute = require("./routes/showRoute");
 const bookRoute = require("./routes/bookingRoute");
 
+const app = express()
+app.use(express.json())
 app.use(cors());
+app.use(express.static("build"))
 app.use("/api/users", userRoute)
 app.use("/api/movies", movieRoute)
 app.use("/api/theaters", theaterRoute)
 app.use("/api/shows", showRoute)
 app.use("/api/booking", bookRoute)
+
+app.get('/profile', function (req, res) {
+    res.redirect('/');
+});
+
 // Common ports are -> (for a server)
 // 5000, 8000, 8080
 // Frontend -> 3000, 5431
